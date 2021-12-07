543af37b60087fed9c940d8e26e42540db18377c
John <johncosta027@gmail.com>
2021-12-07 10:20:45 +0000

    Day 7: 4ms solution

---
+++ Day7/day7.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"math"
+	"strconv"
+	"strings"
+	"time"
+)
+
+func main() {
+	
+	fmt.Println("Day 7")
+	start := time.Now()
+
+	dat, _ := ioutil.ReadFile("day7.txt")
+	
+	stringDat := strings.Split(string(dat), ",")
+	
+	crabList := make([]int, len(stringDat))
+	for i, value := range stringDat {
+		num, _ := strconv.Atoi(value)
+		crabList[i] = num
+	}
+
+	lowest := 100000000000
+	part2lowest := 10000000000000
+
+	for i := 0; i <= 2000; i++ {
+
+		difference := 0
+		part2difference := 0
+
+		for _, n := range crabList {
+			currentDifference := math.Abs(float64(i) - float64(n))
+			difference += int(currentDifference)
+			part2difference += int((currentDifference / 2) * (currentDifference + 1))
+		}
+
+		if difference < lowest {
+			lowest = difference
+		}
+
+		if part2difference < part2lowest {
+			part2lowest = part2difference
+		}
+
+	}
+
+	elapsed := time.Since(start)
+	fmt.Printf("Time Taken: %v \n", elapsed)
+	fmt.Printf("Part 1: %v \n", lowest)
+	fmt.Printf("Part 2: %v \n", part2lowest)
+
+}
