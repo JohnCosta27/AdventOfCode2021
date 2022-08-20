2ace6f58fd7284f7a8f01081abe936fe8755f75f
John Costa <johncosta027@gmail.com>
2022-08-20 14:12:24 +0100

    trivial solution for part 1

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import (
 	"io/ioutil"
 	"time"
 
-	"johncosta.tech/AdventOfCode2021/day13"
+	"johncosta.tech/AdventOfCode2021/day14"
 )
 
 func main() {
@@ -23,44 +23,40 @@ func main() {
 	day5input, _ := ioutil.ReadFile("day5/day5.txt")
 	day6input, _ := ioutil.ReadFile("day6/day6.txt")
 	day7input, _ := ioutil.ReadFile("day7/day7.txt")
-	day8input, _ := ioutil.ReadFile("day8/day8.txt")*/
-	day13input, _ := ioutil.ReadFile("day13/day13.txt")
+	day8input, _ := ioutil.ReadFile("day8/day8.txt")
+	day13input, _ := ioutil.ReadFile("day13/day13.txt")*/
+	day14input, _ := ioutil.ReadFile("day14/day14.txt")
 
 	dayTime := time.Now()
 	/*day1part1, day1part2 := day1.Run(day1input)
 	printAnswers(1, time.Since(dayTime), day1part1, day1part2)
 
-	dayTime = time.Now()
 	day2part1, day2part2 := day2.Run(day2input)
 	printAnswers(2, time.Since(dayTime), day2part1, day2part2)
 
-	dayTime = time.Now()
 	day3part1, day3part2 := day3.Run(day3input)
 	printAnswers(3, time.Since(dayTime), day3part1, day3part2)
 
-	dayTime = time.Now()
 	day4part1, day4part2 := day4.Run(day4input)
 	printAnswers(4, time.Since(dayTime), day4part1, day4part2)
 
-	dayTime = time.Now()
 	day5part1, day5part2 := day5.Run(day5input)
 	printAnswers(5, time.Since(dayTime), day5part1, day5part2)
 
-	dayTime = time.Now()
 	day6part1, day6part2 := day6.Run(day6input)
 	printAnswers(6, time.Since(dayTime), day6part1, day6part2)
 
-	dayTime = time.Now()
 	day7part1, day7part2 := day7.Run(day7input)
 	printAnswers(7, time.Since(dayTime), day7part1, day7part2)
 
-	dayTime = time.Now()
 	day8part1, day8part2 := day6.Run(day8input)
-	printAnswers(8, time.Since(dayTime), day8part1, day8part2)*/
+	printAnswers(8, time.Since(dayTime), day8part1, day8part2)
 
-	dayTime = time.Now()
 	day13part1, _ := day13.Run(day13input)
-	printAnswers(13, time.Since(dayTime), day13part1, 0)
+	printAnswers(13, time.Since(dayTime), day13part1, 0)*/
+
+	day14part1, day14part2 := day14.Run(day14input)
+	printAnswers(8, time.Since(dayTime), day14part1, day14part2)
 
 	elapsed := time.Since(start)
 
@@ -69,7 +65,7 @@ func main() {
 
 }
 
-func printAnswers(day int, time time.Duration, part1 int, part2 int) {
+func printAnswers(day int, time time.Duration, part1 int64, part2 int64) {
 	fmt.Println("---------------------------------------")
 	fmt.Printf("Day %v \n", day)
 	fmt.Printf("Part 1: %v \n", part1)
